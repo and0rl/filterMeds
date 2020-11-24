6a093a6fff498c5fab4a827beb8f732a11178af7
Orlando Ceniccola <44786784+and0rl@users.noreply.github.com>
2020-11-24 02:08:29 +0100

    Update medicines.js

--- src/components/medicines/medicines.js
+++ src/components/medicines/medicines.js
@@ -1,51 +1,44 @@
-import React, { useState } from 'react';
-import styles from './medicines.module.css';
+import React, { useState, useEffect } from "react";
+import styles from "./medicines.module.css";
+
 const Medicines = ({ medicines, searchTerm }) => {
-  console.log(medicines);
+  const [filteredArray, setFilteredArray] = useState([]);
 
-
-  const filteredArray = Medicines.filter((medicine) => {
-      medicine.toLowerCase().includes(searchTerm);
-    if (medicine.name.includes(searchTerm) || medicine.company.includes(searchTerm)) {
-      return medicine;
-    }
-  });
-
-  //   const (ternary) = <condition> ? <ifTrue> : <ifFalse>
-  const arrayToRender = searchTerm ? filteredArray(medicines, searchTerm) : medicines;
-
-  //   function
-
-  arrayToRender.map((m, i) => {
-    return (
-      <div>
-        <medicine key={i} name={m.name} company={m.company} />
-        {''}
-      </div>
+  const filterIt = () => {
+    setFilteredArray(
+      medicines.filter((medicine) => {
+        if (
+          medicine.name.toLowerCase().includes(searchTerm) ||
+          medicine.company.toLowerCase().includes(searchTerm)
+        )
+          return true;
+      })
     );
-  });
-
-  // const arrayToRender = ({ options,})
-
-  const myMedStyle = {
-    textAlign: 'center',
-    margin: '15px',
-    border: '1px solid',
-    padding: '15px',
-    backgroundColor: 'lightgrey',
   };
 
-  if (!medicines) {
+  useEffect(() => {
+    filterIt();
+  }, [searchTerm]);
+
+  const myMedStyle = {
+    textAlign: "center",
+    margin: "15px",
+    border: "1px solid",
+    padding: "15px",
+    backgroundColor: "lightgrey",
+  };
+
+  if (!filteredArray) {
     return <div>no medicines</div>;
   }
 
   return (
     <div className={styles.root}>
-      looks like you have {medicines.length} medicines!
-      {medicines.map((m, i) => (
+      looks like you have {filteredArray.length} medicines!
+      {filteredArray.map((m, i) => (
         <div style={myMedStyle} key={i}>
-          {' '}
-          name: {m.name}, <br /> company: {m.company}{' '}
+          {" "}
+          name: {m.name}, <br /> company: {m.company}{" "}
         </div>
       ))}
     </div>
